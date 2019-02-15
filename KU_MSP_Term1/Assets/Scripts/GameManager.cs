7df9e519f888890ab79e31638f15a5cbc2342daa
AnujPatwari <33886859+anuj-patwari@users.noreply.github.com>
2019-02-15 18:20:14 +0000

    Fixed starting coordinate script

--- KU_MSP_Term1/Assets/Scripts/GameManager.cs
+++ KU_MSP_Term1/Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player.transform.position = startingCoordinates;
+        startingCoordinates = player.transform.position;
     }
 
     // Update is called once per frame
