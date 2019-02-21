efa5906a3073916873d6c56fc1d96fab47217a6c
AnujPatwari <33886859+anuj-patwari@users.noreply.github.com>
2019-02-21 19:36:26 +0000

    Platform creator made.

---
+++ KU_MSP_Term1/Assets/Scripts/ClickablePlatformDefiner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickablePlatformDefiner : MonoBehaviour
+{
+    public Vector2 position;
+    public GameObject platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        GameObject platformToBePlaced = (GameObject)Instantiate(platform, position, transform.rotation);
+        gameObject.SetActive(false);
+    }
+}
