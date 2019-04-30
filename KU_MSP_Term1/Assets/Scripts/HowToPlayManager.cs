fac116a4ed200b106c2299317548f8c6448d7d4e
AnujPatwari <33886859+anuj-patwari@users.noreply.github.com>
2019-04-30 12:48:37 +0530

    Controls added

---
+++ KU_MSP_Term1/Assets/Scripts/HowToPlayManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayManager : MonoBehaviour
+{
+    [SerializeField] GameObject leftButton;
+    [SerializeField] GameObject rightButton;
+    [SerializeField] GameObject howToPlayText;
+    [SerializeField] GameObject controlsText;
+
+    private int menuNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuNumber = 1;
+        leftButton.SetActive(false);
+        howToPlayText.SetActive(true);
+        controlsText.SetActive(false);
+        rightButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchButtonClicked()
+    {
+        if (menuNumber == 1)
+        {
+            rightButton.SetActive(false);
+            howToPlayText.SetActive(false);
+            controlsText.SetActive(true);
+            leftButton.SetActive(true);
+            menuNumber = 2;
+        }
+
+        else if (menuNumber == 2)
+        {
+            leftButton.SetActive(false);
+            howToPlayText.SetActive(true);
+            controlsText.SetActive(false);
+            rightButton.SetActive(true);
+            menuNumber = 1;
+        }
+    }
+}
